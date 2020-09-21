92e18b31558cb69e234585e9050ad7a952679d5c
정찬호 <wjdcksgh3150@gmail.com>
2020-09-22 00:02:57 +0900

    list add

--- arr_exam.java
+++ arr_exam.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class arr_exam {
 
+	public static void list_add(ArrayList list, String str) {
+		list.add(str);
+	}
+	
 	public static void print_list(ArrayList list) {
 		int size = list.size();
 		
@@ -12,7 +16,7 @@ public class arr_exam {
 		}
 		else {
 			for(int i=0; i<size; i++) {
-				System.out.print(i+" : "+list.get(i)+"/n");
+				System.out.print(i+" : "+list.get(i)+", ");
 			}
 		}
 	}
@@ -21,6 +25,10 @@ public class arr_exam {
 		// TODO Auto-generated method stub
 		
 		ArrayList list = new ArrayList<String>();
+		print_list(list);
+		
+		list_add(list,"abc");
+		list_add(list,"def");
 		print_list(list);
 
 	}
